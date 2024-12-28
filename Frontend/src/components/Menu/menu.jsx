4685209ae447c17d08626a70b769cce14459572d
Arushigupta04 <122527185+Arushigupta04@users.noreply.github.com>
2024-12-28 20:52:05 +0530

    Update menu.jsx

--- Frontend/src/components/Menu/menu.jsx
+++ Frontend/src/components/Menu/menu.jsx
@@ -14,7 +14,7 @@ const MenuPage = () => {
   useEffect(() => {
     const fetchItemCategories = async () => {
       try {
-        const response = await axios.get(`${serverURL}/api/add-new/food-items`);
+        const response = await fetch(`${serverURL}/api/add-new/food-items`);
         setItemCategories(response.data);
       } catch (error) {
         console.error('Error fetching item categories:', error);
